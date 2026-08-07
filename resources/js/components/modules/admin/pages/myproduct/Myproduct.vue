177ef26e675bf7715f234c803f173a8689583504
Erwin Setyo Subarkah <erwinsetyo0@gmail.com>
2026-08-08 00:46:17 +0700

    update

--- resources/js/components/modules/admin/pages/myproduct/Myproduct.vue
+++ resources/js/components/modules/admin/pages/myproduct/Myproduct.vue
@@ -24,7 +24,7 @@
 					<tbody></tbody>
 			</table>
 
-			<div v-if="showModal" class="modal fade show d-block" tabindex="-1" role="dialog" aria-modal="true">
+			<div v-if="showModal" class="modal fade show d-block" tabindex="-1" role="dialog" style="overflow:auto;" aria-modal="true">
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
